c265d24e7a0857f1ae29c278f4edc5287f9bc7d3
Ilia Detchenkov <ilia@detchenkov.me>
2024-12-02 15:10:51 +0100

    day 1 part 2

---
+++ day1.2/Program.cs
@@ -0,0 +1,33 @@
+﻿using System.IO;
+
+String line;
+List<int> left = new List<int>();
+List<int> right = new List<int>();
+
+StreamReader sr = new StreamReader("input.txt");
+
+line = sr.ReadLine();
+while (line != null)
+{
+    string[] words = line.Split("   ");
+    left.Add(Int32.Parse(words[0]));
+    right.Add(Int32.Parse(words[1]));
+
+    line = sr.ReadLine();
+}
+sr.Close();
+
+left.Sort();
+
+var sum = 0;
+
+foreach (var item in left)
+{
+    sum += item * right.FindAll(
+        delegate(int e)
+        {
+            return e == item;
+        }).Count();
+}
+
+File.WriteAllText("output.txt", ""+sum);
