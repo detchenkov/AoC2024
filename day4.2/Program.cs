8d52830b0abbe59a8bd1f28236cd51f8962aa7f9
Ilia Detchenkov <ilia@detchenkov.me>
2024-12-05 07:48:54 +0100

    day 4 part 2

---
+++ day4.2/Program.cs
@@ -0,0 +1,46 @@
+﻿using System.IO;
+
+String[] lines = File.ReadAllLines("input.txt");
+
+var count = 0;
+var nb_lines = lines.Count();
+
+for (var i = 0; i < nb_lines; i++)
+{
+    var line_len = lines[i].Length;
+    for (var j = 0; j < line_len; j++)
+    {
+        if (lines[i][j] != 'A') continue;
+        if (j + 1 >= line_len) continue;
+        if (j - 1 < 0) continue;
+        if (i - 1 < 0) continue;
+        if (i + 1 >= nb_lines) continue;
+
+        if (lines[i-1][j-1] != 'M' && lines[i-1][j-1] != 'S')
+            continue;
+        if (lines[i+1][j+1] != 'M' && lines[i+1][j+1] != 'S')
+            continue;
+        if (lines[i-1][j+1] != 'M' && lines[i-1][j+1] != 'S')
+            continue;
+        if (lines[i+1][j-1] != 'M' && lines[i+1][j-1] != 'S')
+            continue;
+
+        if (lines[i-1][j-1] == 'M' && lines[i+1][j+1] == 'S')
+        {
+            if (lines[i+1][j-1] == 'M' && lines[i-1][j+1] == 'S')
+                count++;
+            if (lines[i+1][j-1] == 'S' && lines[i-1][j+1] == 'M')
+                count++;
+        }
+        if (lines[i-1][j-1] == 'S' && lines[i+1][j+1] == 'M')
+        {
+            if (lines[i+1][j-1] == 'M' && lines[i-1][j+1] == 'S')
+                count++;
+            if (lines[i+1][j-1] == 'S' && lines[i-1][j+1] == 'M')
+                count++;
+        }
+    }
+}
+
+File.WriteAllText("output.txt", $"{count}");
+
